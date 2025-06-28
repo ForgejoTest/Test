2038b06e7c6e641928f25bb6e0c6b9540408bb9e
Corte-Z <214324952+Corte-Z@users.noreply.github.com>
2025-06-28 04:18:18 -0400

    pkgs
    Signed-off-by: Corte-Z <214324952+Corte-Z@users.noreply.github.com>

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -5,9 +5,9 @@ set -eux
 sed -i 's/DownloadUser/#DownloadUser/g' /etc/pacman.conf
 
 if [ "$(uname -m)" = 'x86_64' ]; then
-	PKG_TYPE='x86_64.pkg.tar.zst'
+		PKG_TYPE='x86_64.pkg.tar.zst'
 else
-	PKG_TYPE='aarch64.pkg.tar.xz'
+		PKG_TYPE='aarch64.pkg.tar.xz'
 fi
 
 LLVM_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/llvm-libs-mini-$PKG_TYPE"
@@ -20,81 +20,47 @@ MESA_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/
 echo "Installing build dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-	aom \
-	base-devel \
-	boost \
-	boost-libs \
-	catch2 \
-    ccache \
-	cmake \
-	curl \
-	dav1d \
-	desktop-file-utils \
-	doxygen \
-	enet \
-	ffmpeg \
-	ffmpeg4.4 \
-	fmt \
-	gamemode \
-	git \
-	glslang \
-	glu \
-	graphviz \
-	hidapi \
-    jq \
-	libass \
-	libdecor \
-	libfdk-aac \
-	libinih \
-	libopusenc \
-	libva \
-	libvpx \
-	libxi \
-	libxkbcommon-x11 \
-	libxss \
-	libzip \
-	lld \
-	mbedtls \
-	mbedtls2 \
-	mesa \
-	meson \
-	nasm \
-	ninja \
-	nlohmann-json \
-	numactl \
-	pipewire-audio \
-	pulseaudio \
-	pulseaudio-alsa \
-	python-pip \
-	qt6-base \
-	qt6ct \
-	qt6-multimedia \
-	qt6-tools \
-	qt6-wayland \
-    qt6-webengine \
-	rapidjson \
-	sdl2 \
-	spirv-headers \
-	unzip \
-	vulkan-headers \
-	vulkan-mesa-layers \
-	vulkan-nouveau \
-	wget \
-    wireless_tools \
-	x264 \
-	x265 \
-	xcb-util-cursor \
-	xcb-util-image \
-	xcb-util-renderutil \
-	xcb-util-wm \
-	xorg-server-xvfb \
-	zip \
-	zsync
+		base-devel \
+		boost \
+		boost-libs \
+		cmake \
+		curl \
+		enet \
+		fmt \
+		gamemode \
+		git \
+		inetutils \
+		jq \
+		nasm \
+		ninja \
+		nlohmann-json \
+		patchelf \
+		python-pip \
+		qt6-multimedia \
+		qt6-tools \
+		qt6-wayland \
+		qt6-webengine \
+		sdl2 \
+		spirv-headers \
+		strace \
+		unzip \
+		vulkan-headers \
+		vulkan-mesa-layers \
+		vulkan-nouveau \
+		wget \
+		wireless_tools \
+		xcb-util-cursor \
+		xcb-util-image \
+		xcb-util-renderutil \
+		xcb-util-wm \
+		xorg-server-xvfb \
+		zip \
+		zsync
 
 if [ "$(uname -m)" = 'x86_64' ]; then
-	pacman -Syu --noconfirm vulkan-intel haskell-gnutls gcc14 svt-av1 vulkan-radeon
+		pacman -Syu --noconfirm vulkan-intel haskell-gnutls svt-av1 vulkan-radeon
 else
-	pacman -Syu --noconfirm vulkan-freedreno vulkan-panfrost
+		pacman -Syu --noconfirm vulkan-freedreno vulkan-panfrost
 fi
 
 
@@ -103,7 +69,6 @@ echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$LLVM_URL"   -O  ./llvm-libs.pkg.tar.zst
 wget --retry-connrefused --tries=30 "$QT6_URL"    -O  ./qt6-base-iculess.pkg.tar.zst
 wget --retry-connrefused --tries=30 "$LIBXML_URL" -O  ./libxml2-iculess.pkg.tar.zst
-wget --retry-connrefused --tries=30 "$FFMPEG_URL" -O  ./ffmpeg-mini.pkg.tar.zst
 wget --retry-connrefused --tries=30 "$OPUS_URL"   -O  ./opus-nano.pkg.tar.zst
 
 pacman -U --noconfirm ./*.pkg.tar.zst
